dc147ec473464d8dd0f08d4b119b61387a12a516
z-carlson <zcarlson87@gmail.com>
2020-05-17 13:40:23 -0500

    preparing to deploy

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch("http://localhost:3001/imageurl", {
+      fetch("https://cryptic-woodland-51894.herokuapp.com/imageurl", {
         method: 'post',
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({
@@ -88,7 +88,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
           if (response) {
-            fetch("http://localhost:3001/image", {
+            fetch("https://cryptic-woodland-51894.herokuapp.com/image", {
               method: 'put',
               headers: {"Content-Type": "application/json"},
               body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch("http://localhost:3001/signin", {
+        fetch("https://cryptic-woodland-51894.herokuapp.com/signin", {
             method: 'post',
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
